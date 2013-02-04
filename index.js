7593a7b87da643692db0046618b324cac7226239
Mike MacCana <mike.maccana@bazaarvoice.com>
2013-02-04 17:41:41 +0000

    - Use prefixing for most tests.  - Ensure that references in our methods to other agave-provided methods are themselves dynamically prefixed.  - Add Array.toNodeList()  - getParents is now ancestorNodes()  - Readability fixes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Agave.JS
 // I'm a UMD module (works in RequireJS and CommonJS-like environments)
 // See https://github.com/umdjs
 (function (root, factory) {
@@ -15,197 +16,207 @@
   }
 }(this, function () {
 
-  // Agave.JS
-
-  // object.getKeys() returns an array of keys
-  var getKeys = function(){
-    return Object.keys(this);
-  };
-
-  // object.getSize() returns the number of properties in the object
-  var getSize = function() {
-    return Object.keys(this).length;
-  };
-
-  // string.reverse()
-  var reverse = function() {
-    return this.split("").reverse().join("");
-  };
-
-  // string.leftStrip(stripChars) returns the string with the leading chars removed
-  var leftStrip = function(stripChars) {
-    var result = this;
-    while ( true ) {
-      // Note result could be zero characters
-      if ( ! stripChars.contains(result.charAt(0)) || ! result) {
-        return result;
-      } else {
-        result = result.slice(1);
-      }
-    }
-  };
-
-  // string.rightStrip(stripChars) returns the string with the trailing chars removed
-  var rightStrip = function(stripChars) {
-    return this.reverse().leftStrip(stripChars).reverse();
-  };
-
-  // string.strip(stripChars) returns the string with the leading and trailing chars removed
-  var strip = function(stripChars) {
-    return this.leftStrip(stripChars).rightStrip(stripChars);
-  };
-
-  // object.getPath - get the value of the nested keys provided in the object.
-  // If any are missing, return undefined. Used for checking JSON results.
-  var getPath = function(pathItems) {
-    var obj = this;
-    var delim = '/';
-    var result;
-    var still_checking = true;
-    // Handle Unix style paths
-    if ( typeof(pathItems) === 'string' ) {
-      pathItems = pathItems.strip(delim).split(delim);
-    }
-    pathItems.forEach( function(pathItem) {
-      if ( still_checking ) {
-        if ( ! obj.hasOwnProperty(pathItem) ) {
-          result = undefined;
-          still_checking = false;
-        } else {
-          result = obj[pathItem];
-        }
-        obj = obj[pathItem];
-      }
-    });
-    return result;
-  };
-
-  // array.findItem(test_function) returns the first item that matches the test_function
-  var findItem = function(test_function){
-    var arr = this;
-    var last_index;
-    var found = arr.some(function(item, index) {
-      last_index = index;
-      return test_function(item);
-    });
-    if ( found ) {
-      return arr[last_index];
-    } else {
-      return null;
-    }
-  };
-
-  // string.endsWith(suffix) returns true if string ends with the suffix
-  var endsWith = function(suffix) {
-    return this.indexOf(suffix, this.length - suffix.length) !== -1;
-  };
-
-  // string.endsWith(prefix) returns true if string ends with the prefix
-  var startsWith = function(prefix){
-    return this.slice(0, prefix.length) === prefix;
-  };
-
-  // array.contains(item) returns true if an array contains an item
-  // string.contains(substring) returns true if a string contains a substring
-  var contains = function(item){
-    return ( this.indexOf(item) !== -1);
-  };
-
-  // Extend an array with another array.
-  // Cleverness alert: since .apply() accepts an array of args, we use the new_array as all the args to push()
-  var extend = function(new_array) {
-    Array.prototype.push.apply(this, new_array);
-    return this;
-  };
-
-  // string.repeat() repeat a string 'times' times. Borrowed from ES6 shim at https://github.com/paulmillr/es6-shim
-  var repeat = function(times) {
-    if (times < 1) return '';
-    if (times % 2) return this.repeat(times - 1) + this;
-    var half = this.repeat(times / 2);
-    return half + half;
-  };
-
-  // Clone an object recursively
-  var clone = function() {
-    var newObj = (this instanceof Array) ? [] : {};
-    for (var key in this) {
-      if (this[key] && typeof this[key] == "object") {
-        newObj[key] = this[key].clone();
-      } else {
-        newObj[key] = this[key];
-      }
-    }
-    return newObj;
-  };
-
-  var arrayClone = function(){return this.slice();};
-
-  // Add a new element as a child of this element
-  var createChild = function(name, attributes, text) {
-    var newElement = document.createElement(name);
-    if ( attributes ) {
-      for (var attribute in attributes) {
-        newElement.setAttribute(attribute, attributes[attribute]);
-      }
-    }
-    if ( text ) {
-      newElement.textContent = text;
-    }
-    return this.appendChild(newElement);
-  };
-
-  // Apply the CSS styles
-  var applyStyles = function(styles) {
-    for ( var style in styles ) {
-      this.style[style] = styles[style];
-    }
-    return this;
-  };
-
-  // Return array of an elements parent elements from closest to farthest
-  var getParents = function(selector) {
-    var parents = [];
-    var parent = this.parentNode;
-    // While parents are 'element' type nodes
-    // See https://developer.mozilla.org/en-US/docs/DOM/Node.nodeType
-    while ( parent && parent.nodeType && parent.nodeType === 1 ) {
-      if ( selector ) {
-        if ( parent.matches(selector) ) {
-          parents.push(parent);
-        }
-      } else {
-        parents.push(parent);
-      }
-      parent = parent.parentNode;
-    }
-    return parents;
-  };
-
-  // Polyfill if Element.prototype.matches doesn't exist.
-  var prefixedMatchesMethod = ( !this.Element || Element.prototype.msMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.webkitMatchesSelector || Element.prototype.oMatchesSelector);
-
-  // Add method as a non-enumerable property on obj with the name methodName
-  var addMethod = function( obj, prefix, methodName, method) {
-    methodName = prefix ? prefix+methodName: methodName;
-    // Check - NodeLists and Elements don't always exist on all JS implementations
-    if ( obj ) {
-      // Don't add if the method already exists
-      if ( ! obj.methodName ) {
-        Object.defineProperty( obj.prototype, methodName, {value: method, enumerable: false});
-      }
-    }
-  };
-
   // Extend objects with Agave methods, using
   var enable = function(prefix){
     var global = this;
+
+    // object.getKeys() returns an array of keys
+    var getKeys = function(){
+      return Object.keys(this);
+    };
+
+    // object.getSize() returns the number of properties in the object
+    var getSize = function() {
+      return Object.keys(this).length;
+    };
+
+    // string.reverse()
+    var reverse = function() {
+      return this.split("").reverse().join("");
+    };
+
+    // string.leftStrip(stripChars) returns the string with the leading chars removed
+    var leftStrip = function(stripChars) {
+      var result = this;
+      while ( true ) {
+        // Note result could be zero characters
+        if ( ! stripChars[prefix+'contains'](result.charAt(0)) || ! result) {
+          return result;
+        } else {
+          result = result.slice(1);
+        }
+      }
+    };
+
+    // string.rightStrip(stripChars) returns the string with the trailing chars removed
+    var rightStrip = function(stripChars) {
+      return this[prefix+'reverse']()[prefix+'leftStrip'](stripChars)[prefix+'reverse']();
+    };
+
+    // string.strip(stripChars) returns the string with the leading and trailing chars removed
+    var strip = function(stripChars) {
+      return this[prefix+'leftStrip'](stripChars)[prefix+'rightStrip'](stripChars);
+    };
+
+    // object.getPath - get the value of the nested keys provided in the object.
+    // If any are missing, return undefined. Used for checking JSON results.
+    var getPath = function(pathItems) {
+      var obj = this;
+      var delim = '/';
+      var result;
+      var still_checking = true;
+      // Handle Unix style paths
+      if ( typeof(pathItems) === 'string' ) {
+        pathItems = pathItems[prefix+'strip'](delim).split(delim);
+      }
+      pathItems.forEach( function(pathItem) {
+        if ( still_checking ) {
+          if ( ! obj.hasOwnProperty(pathItem) ) {
+            result = undefined;
+            still_checking = false;
+          } else {
+            result = obj[pathItem];
+          }
+          obj = obj[pathItem];
+        }
+      });
+      return result;
+    };
+
+    // array.findItem(test_function) returns the first item that matches the test_function
+    var findItem = function(test_function){
+      var arr = this;
+      var last_index;
+      var found = arr.some(function(item, index) {
+        last_index = index;
+        return test_function(item);
+      });
+      if ( found ) {
+        return arr[last_index];
+      } else {
+        return null;
+      }
+    };
+
+    // string.endsWith(suffix) returns true if string ends with the suffix
+    var endsWith = function(suffix) {
+      return this.indexOf(suffix, this.length - suffix.length) !== -1;
+    };
+
+    // string.endsWith(prefix) returns true if string ends with the prefix
+    var startsWith = function(prefix){
+      return this.slice(0, prefix.length) === prefix;
+    };
+
+    // array.contains(item) returns true if an array contains an item
+    // string.contains(substring) returns true if a string contains a substring
+    var contains = function(item){
+      return ( this.indexOf(item) !== -1);
+    };
+
+    // Extend an array with another array.
+    // Cleverness alert: since .apply() accepts an array of args, we use the new_array as all the args to push()
+    var extend = function(new_array) {
+      Array.prototype.push.apply(this, new_array);
+      return this;
+    };
+
+    // string.repeat() repeat a string 'times' times. Borrowed from ES6 shim at https://github.com/paulmillr/es6-shim
+    var repeat = function(times) {
+      if (times < 1) return '';
+      if (times % 2) return this[prefix+'repeat'](times - 1) + this;
+      var half = this[prefix+'repeat'](times / 2);
+      return half + half;
+    };
+
+    // Clone an object recursively
+    var clone = function() {
+      var newObj = (this instanceof Array) ? [] : {};
+      for (var key in this) {
+        if (this[key] && typeof this[key] == "object") {
+          newObj[key] = this[key][prefix+'clone']();
+        } else {
+          newObj[key] = this[key];
+        }
+      }
+      return newObj;
+    };
+
+    var arrayClone = function(){return this.slice();};
+
+    // Array toNodeList converts arrays to NodeLists
+    var toNodeList = function(){
+      var fragment = document.createDocumentFragment();
+      this.forEach(function(item){
+        fragment.appendChild(item);
+      })
+      return fragment.childNodes;
+    }
+
+    // Add a new element as a child of this element
+    var createChild = function(name, attributes, text) {
+      var newElement = document.createElement(name);
+      if ( attributes ) {
+        for (var attribute in attributes) {
+          newElement.setAttribute(attribute, attributes[attribute]);
+        }
+      }
+      if ( text ) {
+        newElement.textContent = text;
+      }
+      return this.appendChild(newElement);
+    };
+
+    // Apply the CSS styles
+    var applyStyles = function(styles) {
+      for ( var style in styles ) {
+        this.style[style] = styles[style];
+      }
+      return this;
+    };
+
+    // Return nodeList of an elements parent elements from closest to farthest
+    var ancestorNodes = function(selector) {
+      var ancestors = [];
+      var parent = this.parentNode;
+      // While parents are 'element' type nodes
+      // See https://developer.mozilla.org/en-US/docs/DOM/Node.nodeType
+      while ( parent && parent.nodeType && parent.nodeType === 1 ) {
+        if ( selector ) {
+          if ( parent.matches(selector) ) {
+            ancestors.append(parent);
+          }
+        } else {
+          ancestors.append(parent);
+        }
+        parent = parent.parentNode;
+      }
+      // Return a NodeList to be consistent with childNodes
+      return ancestors.toNodeList();
+    };
+
+    // Polyfill if Element.prototype.matches doesn't exist.
+    var prefixedMatchesMethod = ( !this.Element || Element.prototype.msMatchesSelector || Element.prototype.mozMatchesSelector || Element.prototype.webkitMatchesSelector || Element.prototype.oMatchesSelector);
+
+    // Add method as a non-enumerable property on obj with the name methodName
+    var addMethod = function( objectToExtend, prefix, methodName, method) {
+      methodName = prefix ? prefix+methodName: methodName;
+      // Check - NodeLists and Elements don't always exist on all JS implementations
+      if ( objectToExtend ) {
+        // Don't add if the method already exists
+        if ( ! objectToExtend.methodName ) {
+          Object.defineProperty( objectToExtend.prototype, methodName, {value: method, enumerable: false});
+        }
+      }
+    };
+
     var newMethods = {
       'Array':{
         'findItem':findItem,
         'extend':extend,
         'contains':contains,
-        'clone':arrayClone
+        'clone':arrayClone,
+        'toNodeList':toNodeList
       },
       'Object':{
         'getKeys':getKeys,
@@ -227,7 +238,7 @@
       },
       'Element':{
         'createChild':createChild,
-        'getParents':getParents,
+        'ancestorNodes':ancestorNodes,
         'matches':prefixedMatchesMethod,
         'applyStyles':applyStyles
       },
@@ -236,9 +247,9 @@
         'reverse':Array.prototype.reverse
       }
     };
-    for ( var obj in newMethods ) {
-      for ( var method in newMethods[obj] ) {
-        addMethod(global[obj], prefix, method, newMethods[obj][method]);
+    for ( var objectName in newMethods ) {
+      for ( var methodName in newMethods[objectName] ) {
+        addMethod(global[objectName], prefix, methodName, newMethods[objectName][methodName]);
       }
     }
   }.bind();
--- test/tests.js
+++ test/tests.js
@@ -51,98 +51,98 @@ var mockHTML = ' \
 
 setupDOM(mockHTML);
 
-agave.enable();
+agave.enable('agv');
 
 describe('Array.contains', function(){
   it('fetches the item accurately', function(){
-    assert(['one','two','three'].contains('two') );
+    assert(['one','two','three'].agvcontains('two') );
   });
   it('handles missing items accurately', function(){
-    assert( ! ['one','two','three'].contains('notthere') );
+    assert( ! ['one','two','three'].agvcontains('notthere') );
   });
 });
 
 describe('Array.extend', function(){
   it('extends the array accurately', function(){
-    assert.deepEqual([1,2,3].extend([4,5]), [1,2,3,4,5] );
+    assert.deepEqual([1,2,3].agvextend([4,5]), [1,2,3,4,5] );
   });
 });
 
 describe('String.contains', function(){
   it('checks for the substring accurately', function(){
-    assert('elephantine'.contains('tin') );
+    assert('elephantine'.agvcontains('tin') );
   });
   it('handles missing substrings accurately', function(){
-    assert( ! 'elephantine'.contains('zam') );
+    assert( ! 'elephantine'.agvcontains('zam') );
   });
 });
 
 describe('String.endsWith', function(){
   it('works if the string actually ends with the suffix', function(){
-    assert('Hello world'.endsWith('world'));
+    assert('Hello world'.agvendsWith('world'));
   });
   it('handles trying to check if something ends in something larger than itself', function(){
-    assert.equal('world'.endsWith('Hello world'), false);
+    assert.equal('world'.agvendsWith('Hello world'), false);
   });
 });
 
 describe('String.startsWith', function(){
   it('works if the string actually starts with the prefix', function(){
-    assert('Hello world'.startsWith('Hello'));
+    assert('Hello world'.agvstartsWith('Hello'));
   });
 });
 
 describe('String.repeat', function(){
   it('repeats strings accurately', function(){
-    assert.equal('Hello world'.repeat(3), 'Hello worldHello worldHello world');
+    assert.equal('Hello world'.agvrepeat(3), 'Hello worldHello worldHello world');
   });
 });
 
 describe('String.reverse', function(){
   it('reverses strings accurately', function(){
-    assert.equal('Hello world'.reverse(), 'dlrow olleH');
+    assert.equal('Hello world'.agvreverse(), 'dlrow olleH');
   });
 });
 
 describe('String.leftStrip', function(){
   it('strips from the left accurately', function(){
-    assert.equal('Hello world'.leftStrip('Hle'), 'o world');
+    assert.equal('Hello world'.agvleftStrip('Hle'), 'o world');
   });
 });
 
 describe('String.rightStrip', function(){
   it('strips from the right accurately', function(){
-    assert.equal('Hello world'.rightStrip('ldr'), 'Hello wo');
+    assert.equal('Hello world'.agvrightStrip('ldr'), 'Hello wo');
   });
 });
 
 describe('String.rightStrip', function(){
   it('strips from the left accurately with a single character', function(){
-    assert.equal('a'.leftStrip('a'), '');
+    assert.equal('a'.agvleftStrip('a'), '');
   });
 });
 
 describe('String.strip', function(){
   it('strips from the both sides accurately', function(){
-    assert.equal('Hello world'.strip('Hld'), 'ello wor');
+    assert.equal('Hello world'.agvstrip('Hld'), 'ello wor');
   });
 });
 
 describe('Object.getKeys', function(){
   it('fetches keys accurately', function(){
-    assert.deepEqual(mockObject.getKeys(), ["foo","baz","null"] );
+    assert.deepEqual(mockObject.agvgetKeys(), ["foo","baz","null"] );
   });
 });
 
 describe('Object.getSize', function(){
   it('counts keys accurately', function(){
-    assert.equal(mockObject.getSize(), 3);
+    assert.equal(mockObject.agvgetSize(), 3);
   });
 });
 
 describe('Array.findItem', function(){
   it('correctly finds items that match the function', function(){
-    assert.equal(['one','two','three'].findItem(function(item){
+    assert.equal(['one','two','three'].agvfindItem(function(item){
       return (item === 'three');
     }), 'three');
   });
@@ -150,23 +150,23 @@ describe('Array.findItem', function(){
 
 describe('Object.getPath', function(){
   it('returns undefined when a value is missing', function(){
-    assert.equal(mockObject.getPath(['foo','pineapple']), undefined);
+    assert.equal(mockObject.agvgetPath(['foo','pineapple']), undefined);
   });
   it('returns the value when the provided keys exist', function(){
-    assert.equal(mockObject.getPath(['baz','zar','zog']), 'victory');
+    assert.equal(mockObject.agvgetPath(['baz','zar','zog']), 'victory');
   });
   it('returns the value when the provided keys exist, even if null is on the path', function(){
-    assert.equal(mockObject.getPath([null,'yarr','parrot']), 'ahoy');
+    assert.equal(mockObject.agvgetPath([null,'yarr','parrot']), 'ahoy');
   });
   it('works using Unix-style paths', function(){
-    assert.equal(mockObject.getPath('/baz/zar/zog'), 'victory');
+    assert.equal(mockObject.agvgetPath('/baz/zar/zog'), 'victory');
   });
 });
 
 describe('Agave really doesn\'t affect for loops', function(){
   it ('doesn\'t. really', function(){
     for ( var key in mockObject ) {
-      assert( ! ['getKeys','getSize','getPath'].contains(key) );
+      assert( ! ['getKeys','getSize','getPath'].agvcontains(key) );
     }
   });
 });
@@ -182,7 +182,8 @@ describe('NodeList.forEach', function(){
   it('iterates over nodes properly', function(){
     var results = [];
     var paras = document.querySelectorAll('p');
-    paras.forEach(function(para){
+
+    paras.avforEach(function(para){
       results.push(para.textContent)
     })
     var correctResults = [
@@ -198,7 +199,7 @@ describe('NodeList.forEach', function(){
 describe('Element.createChild', function(){
   var sillyText = 'ethical messenger bag';
   var article = document.querySelector('article');
-  article.createChild('p',{'id':'testpara'},sillyText);
+  article.agvcreateChild('p',{'id':'testpara'},sillyText);
   it('creates children with the specified attributes', function(){
     var paraCount = document.querySelector('#testpara');
     assert(paraCount);
@@ -210,9 +211,9 @@ describe('Element.createChild', function(){
 
 describe('Element.getParents', function(){
   it('returns all parent nodes', function(){
-    var ancestors = document.querySelector('heading').getParents();
+    var ancestors = document.querySelector('heading').avgetParents();
     var results = [];
-    ancestors.forEach(function(ancestor){
+    ancestors.avforEach(function(ancestor){
       results.push(ancestor.tagName)
     })
     var correctResults = ["ARTICLE","BODY","HTML"];
@@ -223,7 +224,7 @@ describe('Element.getParents', function(){
 describe('Element.applyStyles', function(){
   it('styles elements', function(){
     var heading = document.querySelector('heading');
-    heading.applyStyles({'font-size':'18em'})
+    heading.agvapplyStyles({'font-size':'18em'})
     assert.equal(heading.style['font-size'], '18em');
   });
 });
